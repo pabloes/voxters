f542eda574f05498c5a6adf49965e40d0b6e2056
Will Caine <will.a.caine@sherwin.com>
2021-06-07 21:16:49 -0400

    adding other features

--- smart-item/voxter/src/game.ts
+++ smart-item/voxter/src/game.ts
@@ -1,12 +1,22 @@
+
 import { Spawner } from '../node_modules/decentraland-builder-scripts/spawner'
-////import Voxter, { Props } from './item'
-////
-////const post = new Voxter()
-////const spawner = new Spawner<Props>(post)
-////
-////spawner.spawn(
-////  'voxter', 
-////  new Transform({ position: new Vector3(8, 1, 8) }), {
-////  tokenId: 32771  
-////})
-////
+import Voxter, { Props } from './item'
+
+const button = new Voxter()
+const spawner = new Spawner<Props>(button)
+
+spawner.spawn(
+  'screen',
+  new Transform({
+    position: new Vector3(4, 0, 8),
+  }),
+  {
+    tokenId: 888,
+    followActive: true,
+    followLockAxis: false,
+    followSpeed: 2,
+    clickable: true,
+    onClickText: "What are you looking at motal?"
+  }
+)
+
--- smart-item/voxter/src/item.ts
+++ smart-item/voxter/src/item.ts
@@ -1,8 +1,24 @@
 import {createVoxter} from "./common/voxter/voxter";
+import { TrackUserFlag } from './common/faceUerSystem'
 export type Props = {
-    tokenId?: number    
+    tokenId?: number,
+    followActive?: boolean,
+    followLockAxis?: boolean,
+    followSpeed?: number,
+    clickable?: boolean,
+    onClick? : Actions,
+    onClickText?: string,
+    clickButton?: ActionButton,
+    showBody?: boolean
   }
    
+declare enum ActionButton {
+  POINTER = 'POINTER',
+  PRIMARY = 'PRIMARY',
+  SECONDARY = 'SECONDARY',
+  ANY = 'ANY'
+}
+
 export default class Voxter implements IScript<Props> {
   init() { 
     new GLTFShape('models/accesories/acc1.glb');
@@ -15,16 +31,73 @@ export default class Voxter implements IScript<Props> {
     new GLTFShape('models/accesories/acc8.glb');
   }
 
-  spawn(host: Entity, props: Props, channel: IChannel) {    
+  spawn(host: Entity, props: Props, channel: IChannel) { 
     const voxter = createVoxter({
           position:new Vector3(0,0.5,0),
           rotation:Quaternion.Zero(),
-          dna:Number(props.tokenId)||32771
+          dna:Number(props.tokenId)||32771,
+          showBody: props.showBody===undefined || props.showBody
     });
     voxter?.setParent(host)
 
+    let turningSpeed:number = Number(props.followSpeed)||2;
+    let isActive:boolean = props.followActive;
+    let lockXZRotation:boolean = props.followLockAxis;
+    
+    host.addComponent(new TrackUserFlag(lockXZRotation, turningSpeed ? turningSpeed : undefined,isActive))
+
+    if(props.clickable){
+      voxter.getEntity().addComponent(
+            new OnPointerDown(
+              () => {
+                //if (this.active[host.name]) {
+                  channel.sendActions(props.onClick)
+                //}
+              },
+              {
+                button: props.clickButton,
+                hoverText: props.onClickText,
+                distance: 6
+              }
+            )
+          )
+    }
+
     channel.request<number>('tokenId', (tokenId:number) => (voxter.applyDna(tokenId) && tokenId))
     channel.reply<number>('tokenId', () => voxter.getDna())
+
+    channel.handleAction('changeTokenId', (action) => {
+      voxter.applyDna(action.values.tokenId)
+    })
+    channel.handleAction('changeRandomTokenId', (action) => {
+      voxter.applyDna(Math.ceil(Math.random()*258047))
+    })
+    channel.handleAction('activateFollow', (action) => {
+      voxter.getEntity().getParent().getComponent(TrackUserFlag).active=true
+    })
+    channel.handleAction('deactivateFollow', (action) => {
+      voxter.getEntity().getParent().getComponent(TrackUserFlag).active=false
+    })
+    channel.handleAction('putOnItem', (action) => {
+      //to enable, must keep track of original host to put back when done
+      //do to host or //voxter.getEntity()?
+      //host.setParent(Attachable.FIRST_PERSON_CAMERA)
+      //host.addComponentOrReplace(new Transform({position:new Vector3(0,0,0)}))
+      //voxter.getEntity().setParent(null);
+      voxter.getEntity().getComponent(Transform).position=new Vector3(0,0,0)
+      voxter.getEntity().setParent(Attachable.FIRST_PERSON_CAMERA);
+    })
+    channel.handleAction('takeOffItem', (action) => {
+      //voxter.getEntity().getParent().getComponent(TrackUserFlag).active=false
+      //voxter.getEntity().setParent(null);
+      voxter.getEntity().setParent(host);
+      voxter.getEntity().getComponent(Transform).position=new Vector3(0,0.5,0)
+      //put down where? where standing on ground? in space? etc. where last left?
+
+      //voxter.getEntity().setParent(Attachable.FIRST_PERSON_CAMERA);
+    })
+                
+
   }
 }
   
